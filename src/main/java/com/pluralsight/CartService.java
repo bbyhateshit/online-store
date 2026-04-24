84e5fdc96ad7b1f201cb44ed7bb0cf44701516ea
bbyhateshit <j.galeta@gmail.com>
2026-04-24 12:56:38 -0400

    Added a method that removes prodcuts from cart.

--- src/main/java/com/pluralsight/CartService.java
+++ src/main/java/com/pluralsight/CartService.java
@@ -10,4 +10,16 @@ public class CartService {
     public void add(Product product) {
         cart.put(product, cart.getOrDefault(product, 0) + 1);
     }
+// Remove a product from the cart
+    public void remove(Product product) {
+        if (cart.containsKey(product)) {
+            int qty = cart.get(product);
+
+            if (qty > 1) {
+                cart.put(product, qty - 1);
+            } else {
+                cart.remove(product);
+            }
+        }
+    }
 }
