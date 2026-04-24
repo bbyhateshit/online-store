1a2cfd1426aea0ba4c0bb5e6c0273933e278b2c1
bbyhateshit <j.galeta@gmail.com>
2026-04-24 11:16:14 -0400

    Inventory loader. So we can access the file, and print out the list.

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -3,7 +3,8 @@ package com.pluralsight;
 public class OnlineStoreApp {
     public static void main(String[] args) {
 
-
+        InventoryService inventory = new InventoryService();
+        inventory.loadProducts("src/main/resources/products.csv");
 
     }
 }
