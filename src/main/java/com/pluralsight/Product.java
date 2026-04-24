d90b8bb804f5dbcc5befbe62873c2edb6b2f8c92
bbyhateshit <j.galeta@gmail.com>
2026-04-24 09:44:17 -0400

    initialized this. notion to separate value when searching for items in the product.csv

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -6,5 +6,10 @@ public class Product {
     private double price;
     private String department;
 
-
+public Product(String sku, String name, double price, String department){
+    this.sku = sku;
+    this.name =name;
+    this.price = price;
+    this.department = department;
+}
 }
