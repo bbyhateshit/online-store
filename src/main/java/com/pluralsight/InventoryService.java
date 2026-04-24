0ad4828d48f98ab85061f9a56ceb90e112c98d51
bbyhateshit <j.galeta@gmail.com>
2026-04-24 10:03:05 -0400

    Bring in the scanner!

--- src/main/java/com/pluralsight/InventoryService.java
+++ src/main/java/com/pluralsight/InventoryService.java
@@ -1,12 +1,15 @@
 package com.pluralsight;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class InventoryService {
     private List<Product> products = new ArrayList<>();
 
     public void loadProducts(String filePath) {
+        try (Scanner scanner = new Scanner(new File("src/main/resources/products.csv")))
 
     }
 }
